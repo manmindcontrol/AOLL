104e5c50d6d889ab30302d3c33d078270e465f24
Samuel Rychvalský <srychvalsky28@gmail.com>
2025-06-25 22:05:24 +0200

    some changes

--- .next/server/chunks/ssr/components_HomePage_tsx_ff153e56._.js
+++ .next/server/chunks/ssr/components_HomePage_tsx_ff153e56._.js
@@ -373,7 +373,7 @@ const HomePage = ()=>{
                                     viewport: {
                                         once: true
                                     },
-                                    className: "bg-white/30 backdrop-blur border-1 border-white/40 p-8 rounded-2xl shadow-lg hover:shadow-xl transition-shadow border border-gray-100",
+                                    className: "bg-white/30 backdrop-blur border-1 border-white/40 p-8 rounded-2xl shadow-lg hover:shadow-xl transition-shado",
                                     children: [
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$3$2e$3_react$2d$dom$40$19$2e$1$2e$0_react$40$19$2e$1$2e$0_$5f$react$40$19$2e$1$2e$0$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                             className: "mb-4",
--- .next/static/chunks/app_layout_tsx_04365424._.js
+++ .next/static/chunks/app_layout_tsx_04365424._.js
@@ -2,8 +2,9 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__9d126714._.css",
+  "static/chunks/4e428_next_dist_5553b750._.js",
   "static/chunks/b8af9_@fortawesome_free-solid-svg-icons_index_mjs_f5e428c4._.js",
-  "static/chunks/node_modules__pnpm_dcb5d6b5._.js",
+  "static/chunks/node_modules__pnpm_ffd1cf51._.js",
   "static/chunks/components_Navbar_tsx_c69926eb._.js"
 ],
     source: "dynamic"
